1bdc02f4cea7aee3e4a1b4ac409457fee1c64220
Faherme46 <faherme46@gmail.com>
2024-10-02 13:30:46 -0500

    third

---
+++ src/app/views/alcohol/alcohol.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alcohol',
+  standalone: true,
+  imports: [],
+  templateUrl: './alcohol.component.html',
+  styleUrl: './alcohol.component.css'
+})
+export class AlcoholComponent {
+
+}
