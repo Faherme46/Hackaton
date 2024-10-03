a4f39a060f3a4a59bbea2a679fa5e8b5c5b79c0a
Faherme46 <faherme46@gmail.com>
2024-10-02 21:25:25 -0500

    firebase

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getFirestore, onSnapshot } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+
+  public firebaseConfig:any;
+  constructor() {
+    this.firebaseConfig = {
+      apiKey: "AIzaSyAzk5jJJIDZc0HKoI_NtLHDIP9u7y-HXEo",
+      authDomain: "hackaton-188e9.firebaseapp.com",
+      projectId: "hackaton-188e9",
+      storageBucket: "hackaton-188e9.appspot.com",
+      messagingSenderId: "662436821049",
+      appId: "1:662436821049:web:05543140281cc38db4f5e8",
+      measurementId: "G-SBD25CY4RH"
+    };
+    // Initialize Firebase
+
+  }
+
+
+  public listenToUsuarios(callback: Function) {
+    const app = initializeApp(this.firebaseConfig);
+    const db = getFirestore(app);
+
+    // Escuchar la colección "Usuarios"
+    const usuariosCollection = collection(db, "Usuarios");
+
+    // `onSnapshot` escucha cualquier cambio en tiempo real en la colección "Usuarios"
+    const unsubscribe = onSnapshot(usuariosCollection, (querySnapshot) => {
+      const list: any[] = [];
+      querySnapshot.forEach((doc) => {
+        list.push(doc.data());
+      });
+
+      // Ejecutar la función callback con los datos actualizados
+      callback(list);
+    });
+
+    // Puedes devolver `unsubscribe` si deseas detener la escucha en algún momento
+    return unsubscribe;
+  }
+
+  public listenToLavado(callback: Function) {
+    const app = initializeApp(this.firebaseConfig);
+    const db = getFirestore(app);
+
+    // Escuchar la colección "Usuarios"
+    const usuariosCollection = collection(db, "LavadoManos");
+
+    // `onSnapshot` escucha cualquier cambio en tiempo real en la colección "Usuarios"
+    const unsubscribe = onSnapshot(usuariosCollection, (querySnapshot) => {
+      const list: any[] = [];
+      querySnapshot.forEach((doc) => {
+        list.push(doc.data());
+      });
+
+      // Ejecutar la función callback con los datos actualizados
+      callback(list);
+    });
+
+    // Puedes devolver `unsubscribe` si deseas detener la escucha en algún momento
+    return unsubscribe;
+  }
+  public listenToAlcohol(callback: Function) {
+    const app = initializeApp(this.firebaseConfig);
+    const db = getFirestore(app);
+
+    // Escuchar la colección "Usuarios"
+    const usuariosCollection = collection(db, "Alcohol");
+
+    // `onSnapshot` escucha cualquier cambio en tiempo real en la colección "Usuarios"
+    const unsubscribe = onSnapshot(usuariosCollection, (querySnapshot) => {
+      const list: any[] = [];
+      querySnapshot.forEach((doc) => {
+        list.push(doc.data());
+      });
+
+      // Ejecutar la función callback con los datos actualizados
+      callback(list);
+    });
+
+    // Puedes devolver `unsubscribe` si deseas detener la escucha en algún momento
+    return unsubscribe;
+  }
+
+}
--- src/app/views/alcohol/alcohol.component.ts
+++ src/app/views/alcohol/alcohol.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { CollectionService } from '../../services/collection.service';
+import { FirebaseService } from '../../services/firebase.service';
+import { CommonModule } from '@angular/common';
+import { Stream } from 'stream';
 @Component({
   selector: 'app-alcohol',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './alcohol.component.html',
   styleUrl: './alcohol.component.css'
 })
 export class AlcoholComponent {
+
+  usuarios: any[] = [];
+  private unsubscribe: any;
+
+
+  constructor(private firebaseService: FirebaseService) {
+
+  }
+  ngOnInit() {
+    // Escuchar actualizaciones en tiempo real de la colección "Usuarios"
+    this.unsubscribe = this.firebaseService.listenToUsuarios((data: any) => {
+      this.usuarios = data;  // Actualizar la lista de usuarios en el componente
+    });
+  }
 
 }
